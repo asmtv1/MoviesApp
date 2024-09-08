c1ea4642e82d9cb1bfe3b9bbc37572e56719c12f
asmtv1 <asmtv1@mail.ru>
2024-09-09 03:02:19 +0400

    добавил звёзды

--- src/assets/Li.jsx
+++ src/assets/Li.jsx
@@ -1,9 +1,10 @@
 import { parseISO, format } from "date-fns";
 import { ru } from "date-fns/locale";
+import { Rate } from "antd";
 
 export default function Li({ film }) {
   let parsedDate;
-
+  console.log(film);
   try {
     parsedDate = format(parseISO(film.release_date), "LLLL d, yyyy", {
       locale: ru,
@@ -12,8 +13,19 @@ export default function Li({ film }) {
     parsedDate = "Дата неизвестна";
   }
 
+  const style = {
+    borderColor:
+      film.vote_average <= 3
+        ? "#E90000"
+        : film.vote_average <= 5
+        ? "#E97E00"
+        : film.vote_average <= 7
+        ? "#F4F400"
+        : "#66E900",
+  };
+
   return (
-    <li>
+    <li className="list">
       <div className="cover">
         <img
           src={
@@ -26,14 +38,20 @@ export default function Li({ film }) {
           alt={`${film.original_title} Film "Logo"`}
         />
       </div>
+
       <div className="info">
         <h1 className="title">{film.original_title}</h1>
+        <div className="rating" style={style}>
+          {film.vote_average.toFixed(1)}
+        </div>
         <p className="release">{parsedDate} </p>
         <div>
           <p className="genre">Action</p>
           <p className="genre">Drama</p>
         </div>
         <p className="description">{film.overview}</p>
+
+        <Rate className="rate" defaultValue={3} />
       </div>
     </li>
   );
